61d338be384b303d3aaeaf510e1fd4ba39bf8576
Brad Nelson <bnelsj@gmail.com>
2015-12-01 12:54:25 -0800

    Added intermediate file options

--- mapping_QC/get_0read_bases.py
+++ mapping_QC/get_0read_bases.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 import tables
 import numpy as np
@@ -17,12 +18,17 @@ MASKED_COUNT_HG19 = 1683340365
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Count bases with 0 wssd read depth (ed 0 only)")
     parser.add_argument("--wssd_files", nargs="+", help="List of wssd_out_files to check")
+    parser.add_argument("--stat_files", nargs="+", help="List of tab-delimited files with sample, contig, and 0-depth bases")
     parser.add_argument("--contigs", nargs="+", help="List of contigs to check")
     parser.add_argument("--masked_count", default="hg19_masked_count.tab", help="Tab delimited file with contig and masked base count (Default: %(default)s)")
-    parser.add_argument("outfile")
+    parser.add_argument("--out_df", help="Write to tab-delimited file")
+    parser.add_argument("--out_pdf", help="Plot pdf")
     parser.add_argument("--corrected", action="store_true")
 
     args = parser.parse_args()
+
+    if args.wssd_files is None and args.stat_files is None:
+        sys.exit("No input files specified. Use --wssd_files or --stat_files.")
 
     masked_dict = {}
     with open(args.masked_count, "r") as reader:
@@ -43,39 +49,66 @@ if __name__ == "__main__":
     else:
         root = "depthAndStarts_wssd"
 
-    entries = range(len(args.wssd_files) * (len(args.contigs) + 1))
+    n_wssd = len(args.wssd_files) if args.wssd_files is not None else 0
+    n_stat = len(args.stat_files) if args.stat_files is not None else 0
+    entries = range((n_wssd + n_stat) * (len(args.contigs) + 1))
     wssd_dat = pd.DataFrame(index = entries, 
                             columns = ["SAMPLE", "CONTIG", "NZERO"])
 
     i = 0
-    for fn in args.wssd_files:
-        wssd = tables.File(fn, "r")
-        sn = os.path.basename(os.path.dirname(fn))
-        print "%s\t%s" % (sn, fn)
-        z_sum = 0
-        for contig in args.contigs:
-            print "\t%s" % contig
-            contig_rd = wssd.getNode("/%s/%s" % (root, contig))
-            contig_rd.shape[0]
-            sum = contig_rd.shape[0] - np.count_nonzero(contig_rd[:, 0, 0])
-            wssd_dat.loc[i] = [sn, contig, sum]
-            z_sum += sum
+    if args.wssd_files is not None:
+        for fn in args.wssd_files:
+            wssd = tables.File(fn, "r")
+            sn = os.path.basename(os.path.dirname(fn))
+            print "%s\t%s" % (sn, fn)
+            z_sum = 0
+            for contig in args.contigs:
+                print "\t%s" % contig
+                contig_rd = wssd.getNode("/%s/%s" % (root, contig))
+                contig_rd.shape[0]
+                sum = contig_rd.shape[0] - np.count_nonzero(contig_rd[:, 0, 0])
+                wssd_dat.loc[i] = [sn, contig, sum]
+                z_sum += sum
+                i += 1
+            wssd_dat.loc[i] = [sn, "all", z_sum]
             i += 1
-        wssd_dat.loc[i] = [sn, "all", z_sum]
-        i += 1
-        wssd.close()
+            wssd.close()
 
-    f, ax = plt.subplots(figsize=(6, 0.5*len(args.wssd_files)+1))
-    sns.set_color_codes("pastel")
-    wssd_dat_summary = wssd_dat.ix[wssd_dat.CONTIG == "all",]
-    sns.barplot(x="NZERO", y="SAMPLE", data = wssd_dat_summary.sort("NZERO", ascending=False))
-    ax.set_title("Zero-depth base count QC")
-    ticks = ax.get_xticks().tolist()
-    tick_labels = map(lambda x: round(x/1000000., 1), ticks)
-    plt.xticks(ticks, tick_labels)
+    stat_dict = {}
 
-    ax.set(ylabel = "", xlabel="Count of 0-depth bases (Mb)")
-    plt.axvline(x=n_masked, linewidth=2, color="k", label="hg19 masked bases")
-    #sns.despine(left=True)
-    plt.tight_layout()
-    plt.savefig(args.outfile)
+    if args.stat_files is not None:
+        for fn in args.stat_files:
+            with open(fn, "r") as reader:
+                for line in reader:
+                    sn, contig, nzero = line.rstrip().split()
+                    if sn not in stat_dict:
+                        stat_dict[sn] = {}
+                    stat_dict[sn][contig] = int(nzero)
+
+        for sn in stat_dict.keys():
+            tot = 0
+            for contig in args.contigs:
+                wssd_dat.loc[i] = [sn, contig, stat_dict[sn][contig]]
+                tot += stat_dict[sn][contig]
+                i += 1
+            wssd_dat.loc[i] = [sn, "all", tot]
+            i += 1
+
+    if args.out_df is not None:
+        wssd_dat.ix[wssd_dat.CONTIG != "all",].to_csv(args.out_df, sep="\t", header=False, index=False)
+
+    if args.out_pdf is not None:
+        f, ax = plt.subplots(figsize=(6, 0.5*len(wssd_dat.SAMPLE.unique())+1))
+        sns.set_color_codes("pastel")
+        wssd_dat_summary = wssd_dat.ix[wssd_dat.CONTIG == "all",]
+        sns.barplot(x="NZERO", y="SAMPLE", data = wssd_dat_summary.sort("NZERO", ascending=False))
+        ax.set_title("Zero-depth base count QC")
+        ticks = ax.get_xticks().tolist()
+        tick_labels = map(lambda x: round(x/1000000., 1), ticks)
+        plt.xticks(ticks, tick_labels)
+
+        ax.set(ylabel = "", xlabel="Count of 0-depth bases (Mb)")
+        plt.axvline(x=n_masked, linewidth=2, color="k", label="hg19 masked bases")
+        #sns.despine(left=True)
+        plt.tight_layout()
+        plt.savefig(args.out_pdf)
