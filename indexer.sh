26e0eb1289b42dbec1876a0ecc93dbdb4b3edda8
William Harvey <williamthomasharvey@gmail.com>
2019-07-02 16:38:55 -0700

    Added indexing script

---
+++ indexer.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+
+mkdir index
+path=`pwd`
+for file in `ls mapping`; do echo -e "$file\t$path/mapping\t$path/bac_analysis\t$path/sequences\t$path/primary_analysis" > index/$file; done
+cat index/* > index.genome
