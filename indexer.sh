0708736ba7665985d86657dea475ebb8f88f7295
William Harvey <williamthomasharvey@gmail.com>
2019-07-03 10:04:06 -0700

    test

--- indexer.sh
+++ indexer.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
+test
+
 mkdir index
 path=`pwd`
 for file in `ls mapping`; do echo -e "$file\t$path/mapping\t$path/bac_analysis\t$path/sequences\t$path/primary_analysis" > index/$file; done
